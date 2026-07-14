5db0abb308781d0230cc3a579eac272a2fe4d6e1
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2026-07-14 17:16:32 +0300

    prisma client changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -10,8 +10,9 @@
 
 // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-import { PrismaClient } from "../app/generated/prisma/client";
+// import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as {
 	prisma: PrismaClient;
 };
