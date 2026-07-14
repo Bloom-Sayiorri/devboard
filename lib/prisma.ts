9f619b960f4d1bb119ba5731206815719742448d
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2026-07-14 17:20:24 +0300

    prisma client import fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,5 @@
-// import { PrismaClient } from "@/app/generated/prisma";
-
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-// export const prisma =
-// 	globalForPrisma.prisma ||
-// 	new PrismaClient({
-// 		log: ["query", "error", "warn"],
-// 	});
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-// import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "../app/generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as {
 	prisma: PrismaClient;
 };
